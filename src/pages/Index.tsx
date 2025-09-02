e1e92dc4fa196e4b1d4b070ab008889b34fabd0e
Flyzap <164262909+Flyzap@users.noreply.github.com>
2025-09-02 14:35:22 -0300

    Remove seção de depoimentos sociais
    - Remove SocialProofSection.tsx completamente
    - Atualiza Index.tsx removendo importação e uso do componente
    - Remove referências a '5.000+ médicos treinados' e depoimentos
    - Adequa landing page para fase pré-lançamento sem turmas formadas
    - Mantém estrutura limpa e focada no conteúdo atual

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import LeadMagnetSection from "@/components/LeadMagnetSection";
 import InstructorSection from "@/components/InstructorSection";
-import SocialProofSection from "@/components/SocialProofSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import ObjectionsSection from "@/components/ObjectionsSection";
 import TransformationSection from "@/components/TransformationSection";
@@ -14,7 +13,6 @@ const Index = () => {
       <HeroSection />
       <LeadMagnetSection />
       <InstructorSection />
-      <SocialProofSection />
       <BenefitsSection />
       <ObjectionsSection />
       <TransformationSection />
